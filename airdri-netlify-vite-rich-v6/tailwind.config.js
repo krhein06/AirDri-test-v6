15ff2125953456cac340cf96e5736abe44e674e5
krhein06 <118383695+krhein06@users.noreply.github.com>
2025-09-29 09:49:37 -0500

    Delete airdri-netlify-vite-rich-v6 directory

--- airdri-netlify-vite-rich-v6/tailwind.config.js
+++
@@ -1,2 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'], theme:{ extend:{ colors:{ brand:'#29265b' }, fontFamily:{ sans:['Segoe UI','SegoeUI','Roboto','Helvetica','Arial','sans-serif'] } } }, plugins:[] }
